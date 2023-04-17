bfa4c8df97d287d7df84745f2f18bca01770dcd3
hugo20016 <90892100+hugo20016@users.noreply.github.com>
2023-04-17 18:34:14 +0200

    El código corre, pero hay que echarle un ojo a cómo queremos llamar a cada cosa HUGO

--- api.py
+++ api.py
@@ -2,8 +2,12 @@
 
 from flask import Flask, request, render_template
 import requests
-app = Flask(__name__)
 
+app = Flask(__name__)
+comprobador_Usuario = False
+comprobador_Producto = False
+comprobador_Pedido = False
+comprobador_OrderDetail = False
 
 @app.route('/hola')
 def hola():
@@ -32,49 +36,90 @@ def enviar_datos():
             host="gachia-db.cqhzxnm4nr1w.eu-west-1.rds.amazonaws.com",
             username="admin",
             password="gachiaproyecto23",
-            database="gachia-db",
+            database="gachia_gs",
             port="3306"
         )
         
     mycursor = mydb.cursor()
 
-    # Comprobar que el User_ID existe en la tabla Usuarios
-
+    # Comprobar que si el User_ID existe en la BBDD. Si es así, inserta los datos correspondientes en la tabla Usuarios.
+    
     mycursor.execute("SELECT * FROM Usuarios WHERE ID = %s", (user_id,))
     myresult = mycursor.fetchall()
 
     if len(myresult) == 0:
         return 'El usuario no existe'
     else:
-        sql = "INSERT INTO Usuarios (ID, Product_ID, Quantity, UnitPrice) VALUES (%s, %s, %s, %s)"
-        val = (user_id, product_id, quantity, unit_price)
+        sql = "INSERT INTO Usuarios (ID) VALUES (%s)"
+        val = (user_id,)
 
         mycursor.execute(sql, val)
 
         mydb.commit()
+        comprobador_Usuario = True
 
-    # Comprobar que el Product_ID no existe en la tabla Pedidos
+    # Crea un nuevo Pedido en la tabla Pedidos con los datos correspondientes.
 
-    mycursor.execute("SELECT * FROM Pedidos WHERE Product_ID = %s", (product_id,))
+
+    sql = "INSERT INTO Pedidos (User_ID, ID, CreationDate, Status) VALUES (%s, %s, %s, %s)"
+
+    # El valor del ID del pedido se genera automáticamente en la BBDD, cogiendo el último valor y sumándole 1 a este.
+
+    mycursor.execute("SELECT MAX(ID) FROM Pedidos")
+    myresult = mycursor.fetchall()
+    for x in myresult:
+        id_pedido = x[0] + 1
+
+    val = (user_id, id_pedido, creation_date, status)
+
+    mycursor.execute(sql, val)
+
+    mydb.commit()
+
+    comprobador_Pedido = True
+
+    # Crea un nuevo OrderDetail en la tabla OrderDetails con los datos correspondientes.
+
+    sql = "INSERT INTO OrderDetails (Order_ID, Product_ID, Quantity, UnitPrice) VALUES (%s, %s, %s, %s)"
+
+    # El valor del ID del pedido es el mismo que el ID del pedido que se ha creado en la tabla Pedidos.
+    mycursor.execute("SELECT MAX(ID) FROM Pedidos")
 
     myresult = mycursor.fetchall()
 
-    if len(myresult) != 0:
-        return 'El producto ya existe'
+    for x in myresult:
+        id_pedido = x[0]
+
+    val = (id_pedido, product_id, quantity, unit_price)
+
+    mycursor.execute(sql, val)
+
+    mydb.commit()
+
+    comprobador_OrderDetail = True
+
+    # Comprobar que si el Product_ID existe en la BBDD. Si es así, inserta los datos correspondientes en la tabla Productos.
+
+    mycursor.execute("SELECT * FROM Productos WHERE ID = %s", (product_id,))
+
+    myresult = mycursor.fetchall()
+
+    if len(myresult) == 0:
+        return 'El producto no existe'
     else:
-        sql = "INSERT INTO Pedidos (ID, User_ID, CreationDate, Product_ID, Status) VALUES (%s, %s, %s, %s, %s)"
-        val = (product_id, user_id, creation_date, product_id, status)
+        sql = "INSERT INTO Productos (ID, Quantity, UnitPrice) VALUES (%s, %s, %s)"
+        val = (product_id, quantity, unit_price)
 
         mycursor.execute(sql, val)
 
         mydb.commit()
+
+        comprobador_Producto = True
     
-    
-
-
-
-    # Devolver una respuesta al cliente
-    return 'Datos recibidos'
+    if comprobador_Usuario == True and comprobador_Producto == True and comprobador_Pedido == True and comprobador_OrderDetail == True:
+        return 'Datos insertados correctamente'
+    else:
+        return 'Ha habido un error'
 if __name__ == '__main__':
     app.run(debug=True)
 
